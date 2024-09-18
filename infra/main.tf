1c203dd1770795546646dd4239455a214bf1c927
caio367 <caio.antuns@gmail.com>
2024-09-18 03:20:39 -0300

    Creating api gateway

--- infra/main.tf
+++ infra/main.tf
@@ -1,36 +1,45 @@
 terraform {
   backend "s3" {
     bucket = "terraform-deploy-fiap2024"
-    key    = "backend/terraform.tfstate"
+    key    = "backend/gtw/terraform.tfstate"
     region = "us-east-1"
   }
 }
 
-resource "aws_db_instance" "rdstest" {
-  identifier           = "rdstest"
-  allocated_storage    = 5
-  storage_type         = "gp2"
-  engine               = "postgres"
-  engine_version       = "16.3"
-  instance_class       = "db.t3.micro"
-  db_name              = var.DB_NAME
-  username             = var.DB_USERNAME
-  password             = var.DB_PASSWORD
-  parameter_group_name = "default.postgres16"
-  skip_final_snapshot  = true
-  publicly_accessible  = true
-  db_subnet_group_name   = aws_db_subnet_group.rdstest_db_subnet_gp.name
-  vpc_security_group_ids = [aws_security_group.rdstest_db_sg.id]
+resource "aws_api_gateway_rest_api" "fastfood_api_gtw" {
+  name        = "fastfood_api_gtw"
+  description = "FastFood API Gateway"
 
   tags = {
-    Name = "rdstest_instance"
+    Name = "fastfood_api_gtw"
   }
 }
 
-resource "null_resource" "db_schema" {
-  depends_on = [aws_db_instance.rdstest]
+resource "aws_api_gateway_resource" "gtw_resource" {
+  rest_api_id = aws_api_gateway_rest_api.fastfood_api_gtw.id
+  parent_id   = aws_api_gateway_rest_api.fastfood_api_gtw.root_resource_id
+  path_part   = "{proxy+}"
+}
 
-  provisioner "local-exec" {
-    command = "export PGPASSWORD=${var.DB_PASSWORD}; sleep 60; psql -U ${var.DB_USERNAME} -d ${var.DB_NAME} -h ${aws_db_instance.rdstest.address} -f ./db_schema.sql"
-  }
+resource "aws_api_gateway_authorizer" "lambda_authorizer" {
+  name                   = "lambda_authorizer"
+  rest_api_id            = aws_api_gateway_rest_api.fastfood_api_gtw.id
+  authorizer_uri         = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-1:064151784429:function:fiap-tech-auth-dev-api/invocations"
+  authorizer_credentials = "arn:aws:iam::064151784429:role/LabRole"
+  type                   = "REQUEST"
+}
+
+resource "aws_api_gateway_method" "gtw_method" {
+  rest_api_id   = aws_api_gateway_rest_api.fastfood_api_gtw.id
+  resource_id   = aws_api_gateway_resource.gtw_resource.id
+  http_method   = "ANY"
+  authorization = "CUSTOM"
+  authorizer_id = aws_api_gateway_authorizer.lambda_authorizer.id
+}
+
+resource "aws_api_gateway_integration" "gtw_integration" {
+  rest_api_id = aws_api_gateway_rest_api.fastfood_api_gtw.id
+  resource_id = aws_api_gateway_resource.gtw_resource.id
+  http_method = aws_api_gateway_method.gtw_method.http_method
+  type        = "MOCK"
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,19 +1,3 @@
-variable "DB_NAME" {
-  description = "The name of the database"
-  type        = string
-}
-
-variable "DB_USERNAME" {
-  description = "The username for the database"
-  type        = string
-}
-
-variable "DB_PASSWORD" {
-  description = "The password for the database"
-  type        = string
-  sensitive   = true
-}
-
 variable "AWS_ACCESS_KEY_ID" {
   description = "The AWS access key"
   type        = string
@@ -35,38 +19,4 @@ variable "AWS_SESSION_TOKEN" {
 variable "AWS_REGION" {
   description = "The AWS region"
   type        = string
-}
-
-variable "SUBNET_AZ_1" {
-  description = "First availability zone for the subnet (AZs-1)"
-  type        = string
-}
-
-variable "SUBNET_AZ_2" {
-  description = "Second availability zone for the subnet (AZs-2)"
-  type        = string
-}
-
-variable "vpc_cidr_block" {
-  description = "cidr block for VPC"
-  type        = string
-  default     = "10.0.0.0/24"
-}
-
-variable "db_subnet_cidr_block_1" {
-  description = "cidr block for db subnet"
-  type        = string
-  default     = "10.0.0.0/25"
-}
-
-variable "db_subnet_cidr_block_2" {
-  description = "cidr block for db subnet"
-  type        = string
-  default     = "10.0.0.128/25"
-}
-
-variable "GIT_TOKEN" {
-  description = "Token for github"
-  type        = string
-  sensitive   = true
 }
