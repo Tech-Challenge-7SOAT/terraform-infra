41fdf5b85d9818220b1c9ef65329b796ad8802e7
caio367 <caio.antuns@gmail.com>
2024-09-25 00:24:34 -0300

    Updating gateway responses

--- infra/gtwresponses.tf
+++ infra/gtwresponses.tf
@@ -7,27 +7,27 @@
 #  }
 #}
 
-resource "aws_api_gateway_method_response" "method_response" {
-  count       = length(var.status_codes)
-  rest_api_id = aws_api_gateway_rest_api.fastfood_api_gtw.id
-  resource_id = aws_api_gateway_resource.gtw_resource.id
-  http_method = aws_api_gateway_method.gtw_method.http_method
-  status_code = var.status_codes[count.index]
-}
-
-resource "aws_api_gateway_integration_response" "integration_response" {
-  count       = length(var.status_codes)
-  rest_api_id = aws_api_gateway_rest_api.fastfood_api_gtw.id
-  resource_id = aws_api_gateway_resource.gtw_resource.id
-  http_method = aws_api_gateway_method.gtw_method.http_method
-  status_code = var.status_codes[count.index]
-
-  selection_pattern = count.index == 0 ? "default" : "^${var.status_codes[count.index]}"
-
-  response_templates = {
-    "application/json" = ""
-  }
-}
+#resource "aws_api_gateway_method_response" "method_response" {
+#  count       = length(var.status_codes)
+#  rest_api_id = aws_api_gateway_rest_api.fastfood_api_gtw.id
+#  resource_id = aws_api_gateway_resource.gtw_resource.id
+#  http_method = aws_api_gateway_method.gtw_method.http_method
+#  status_code = var.status_codes[count.index]
+#}
+#
+#resource "aws_api_gateway_integration_response" "integration_response" {
+#  count       = length(var.status_codes)
+#  rest_api_id = aws_api_gateway_rest_api.fastfood_api_gtw.id
+#  resource_id = aws_api_gateway_resource.gtw_resource.id
+#  http_method = aws_api_gateway_method.gtw_method.http_method
+#  status_code = var.status_codes[count.index]
+#
+#  selection_pattern = count.index == 0 ? "default" : "^${var.status_codes[count.index]}"
+#
+#  response_templates = {
+#    "application/json" = ""
+#  }
+#}
 
 #resource "aws_api_gateway_gateway_response" "default_4xx" {
 #  rest_api_id   = aws_api_gateway_rest_api.fastfood_api_gtw.id
