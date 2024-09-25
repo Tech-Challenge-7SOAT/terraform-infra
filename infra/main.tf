383588ef45bfa4ba9a063f5651c8a2cf415ce6b9
caio367 <caio.antuns@gmail.com>
2024-09-25 20:47:06 -0300

    Updating gateway responses

--- infra/main.tf
+++ infra/main.tf
@@ -60,7 +60,7 @@ resource "aws_api_gateway_integration_response" "response_integ_mock" {
   rest_api_id = aws_api_gateway_rest_api.fastfood_api_gtw.id
   resource_id = aws_api_gateway_resource.gtw_resource.id
   http_method = aws_api_gateway_method.gtw_method.http_method
-  status_code = aws_api_gateway_method_response.response_200.status_code
+  status_code = "200"
 
   response_templates = {
     "application/json" = "Empty"
