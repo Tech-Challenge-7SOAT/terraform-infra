ef7714a91d51abdbf9e4efa69dc1bf14dca5b307
caio367 <caio.antuns@gmail.com>
2024-09-25 21:01:24 -0300

    Updating gateway responses

--- infra/main.tf
+++ infra/main.tf
@@ -56,16 +56,16 @@ resource "aws_api_gateway_method_response" "response_200" {
   status_code = "200"
 }
 
-resource "aws_api_gateway_integration_response" "response_integ_mock" {
-  rest_api_id = aws_api_gateway_rest_api.fastfood_api_gtw.id
-  resource_id = aws_api_gateway_resource.gtw_resource.id
-  http_method = aws_api_gateway_method.gtw_method.http_method
-  status_code = "200"
-
-  response_templates = {
-    "application/json" = "Empty"
-  }
-}
+#resource "aws_api_gateway_integration_response" "response_integ_mock" {
+#  rest_api_id = aws_api_gateway_rest_api.fastfood_api_gtw.id
+#  resource_id = aws_api_gateway_resource.gtw_resource.id
+#  http_method = aws_api_gateway_method.gtw_method.http_method
+#  status_code = "200"
+#
+#  response_templates = {
+#    "application/json" = "Empty"
+#  }
+#}
 
 #resource "aws_apigatewayv2_vpc_link" "fastfood_gtw_vpc_link" {
 #  name        = "${var.api_name}-vpc_link"
