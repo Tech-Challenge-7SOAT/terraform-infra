bc85f3908248db5babc20cceb2ccfd9c884292ba
caio367 <caio.antuns@gmail.com>
2024-09-27 21:23:21 -0300

    Updating gateway responses

--- infra/gtwresponses.tf
+++ infra/gtwresponses.tf
@@ -11,6 +11,8 @@ resource "aws_api_gateway_integration_response" "response_integ_200" {
   http_method = aws_api_gateway_method.gtw_method.http_method
   status_code = aws_api_gateway_method_response.response_200.status_code
 
+  selection_pattern = "2[0-9]{2}"
+
   response_templates = {
     "application/json" = ""
   }
@@ -28,6 +30,8 @@ resource "aws_api_gateway_integration_response" "response_integ_403" {
   resource_id = aws_api_gateway_resource.gtw_resource.id
   http_method = aws_api_gateway_method.gtw_method.http_method
   status_code = aws_api_gateway_method_response.response_403.status_code
+
+  selection_pattern = "403"
 
   response_templates = {
     "application/json" = ""
